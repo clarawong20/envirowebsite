81d266f166d5d82b4134a0d537f434d1d30c4532
clarawong20 <57604770+clarawong20@users.noreply.github.com>
2021-06-22 09:41:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+//This will bring the popup window
+function basicPopup(url) {
+  popupWindow = window.open(url,'popUpWindow','height=500,width=800,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
+}
